11fc53de859ab8e06abca3a75621fcf4e5a4d951
Jack Conner <1jackconner@gmail.com>
2024-04-17 20:41:01 -0700

    create constants file

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+from enum import StrEnum
+
+
+class TextTypes(StrEnum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class HTMLTags(StrEnum):
+    BOLD = "b"
+    ITALIC = "i"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class HTMLProps(StrEnum):
+    LINK = "href"
+    IMAGE_SRC = "src"
+    ALT_TEXT = "alt"
--- src/main.py
+++ src/main.py
@@ -1,33 +1,52 @@
 from textnode import TextNode
 from htmlnode import LeafNode
+from constants import TextTypes, HTMLTags, HTMLProps
 
 
 def text_node_to_html_node(text_node: TextNode):
-    VALID_TEXT_NODE_TYPES = ["text", "bold", "italic", "code", "link", "image"]
+    # VALID_TEXT_NODE_TYPES = [
+    #     TextTypes.TEXT,
+    #     TextTypes.BOLD,
+    #     TextTypes.ITALIC,
+    #     TextTypes.CODE,
+    #     TextTypes.LINK,
+    #     TextTypes.IMAGE,
+    # ]
 
-    if text_node.text_type not in VALID_TEXT_NODE_TYPES:
+    if text_node.text_type in TextTypes:
         raise Exception(f"TextNode of type {text_node.text_type} is not valid.")
 
-    if text_node.text_type == "text":
+    if text_node.text_type == TextTypes.TEXT:
         return LeafNode(value=text_node.text)
-    if text_node.text_type == "bold":
-        return LeafNode(tag="b", value=text_node.text)
-    if text_node.text_type == "italic":
-        return LeafNode(tag="i", value=text_node.text)
-    if text_node.text_type == "code":
-        return LeafNode(tag="code", value=text_node.text)
-    if text_node.text_type == "link":
+    if text_node.text_type == TextTypes.BOLD:
+        return LeafNode(tag=HTMLTags.BOLD, value=text_node.text)
+    if text_node.text_type == TextTypes.ITALIC:
+        return LeafNode(tag=HTMLTags.ITALIC, value=text_node.text)
+    if text_node.text_type == TextTypes.CODE:
+        return LeafNode(tag=HTMLTags.CODE, value=text_node.text)
+    if text_node.text_type == TextTypes.LINK:
         return LeafNode(
-            tag="a",
+            tag=HTMLTags.LINK,
             value=text_node.text,
-            props={"href": text_node.url},
+            props={HTMLProps.LINK: text_node.url},
         )
-    if text_node.text_type == "image":
+    if text_node.text_type == TextTypes.IMAGE:
         return LeafNode(
-            tag="img",
+            tag=HTMLTags.IMAGE,
             value=text_node.text,
-            props={"src": text_node.url, "alt": text_node.text},
+            props={
+                HTMLProps.IMAGE_SRC: text_node.url,
+                HTMLProps.ALT_TEXT: text_node.text,
+            },
         )
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type
+) -> list[TextNode]:
+    # new_nodes = []
+    # for node in old_nodes:
+    raise NotImplementedError("xD")
 
 
 def main():
